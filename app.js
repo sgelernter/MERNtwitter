23dc58fec6fd4bb2af623cbe56c8ad4edca6838b
sam <s.gelernter@gmail.com>
2022-01-16 18:55:55 -0500

    Setup repo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+mongoose 
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Goodbye World'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
