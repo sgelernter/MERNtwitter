fb84e19c6e4591392ff30273dca6660e8e91c7a7
sam <s.gelernter@gmail.com>
2022-01-16 20:22:04 -0500

    User auth setup

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require('passport');
 const port = process.env.PORT || 5000;
 
 mongoose 
@@ -9,7 +13,12 @@ mongoose
     .then(() => console.log('Connected to MongoDB successfully'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Goodbye World'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
