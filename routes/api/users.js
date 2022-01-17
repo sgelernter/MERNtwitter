ed6922d708305648f965693524c4e3508334df2b
sam <s.gelernter@gmail.com>
2022-01-17 01:50:34 -0500

    Begin React setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,16 +5,26 @@ const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const validateLoginInput = require('../../validation/login');
+const validateRegisterInput = require('../../validation/register');
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
 
 
 // User registration
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                return res.status(400).json({email: 'A user has already registered with that email'})
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors);
             } else {
                 const newUser = new User({
                     handle: req.body.handle,
@@ -50,10 +60,17 @@ router.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({email})
         .then(user => {
             if (!user) {
-                return res.status(404).json({email: 'This user does not exist'});
+                errors.email = 'This user does not exist';
+                return res.status(404).json(errors);
             }
 
             bcrypt.compare(password, user.password)
@@ -72,7 +89,8 @@ router.post('/login', (req, res) => {
                                 });
                             });
                     } else {
-                        return res.status(400).json({password: 'Incorrect password'});
+                        errors.password = 'Incorrect password';
+                        return res.status(400).json(errors);
                     }
                 })
         })
